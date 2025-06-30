ef7ea7d5e297b4ee6df2083182ec6068e5d2fae1
Artur <Mouter91@gmail.com>
2025-06-30 12:14:01 +0300

    Тестирование глубины

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,6 +1,6 @@
 #include "config/config.h"
-#include "shader/shader.h"
 #include "model/model.h"
+#include "shader/shader.h"
 
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
@@ -16,7 +16,8 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   GLFWwindow *window =
-      glfwCreateWindow(WindowConfig::width, WindowConfig::height, WindowConfig::title, NULL, NULL);
+      glfwCreateWindow(WindowConfig::width, WindowConfig::height,
+                       WindowConfig::title, NULL, NULL);
   if (window == nullptr) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
@@ -24,8 +25,10 @@ int main() {
   }
 
   glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(
-      window, [](GLFWwindow *, int width, int height) { glViewport(0, 0, width, height); });
+  glfwSetFramebufferSizeCallback(window,
+                                 [](GLFWwindow *, int width, int height) {
+                                   glViewport(0, 0, width, height);
+                                 });
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
@@ -42,40 +45,111 @@ int main() {
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   glfwSetWindowUserPointer(window, &camera);
 
-  glfwSetCursorPosCallback(window, [](GLFWwindow *win, double xpos, double ypos) {
-    static bool firstMouse = true;
-    static float lastX = static_cast<float>(WindowConfig::width) / 2,
-                 lastY = static_cast<float>(WindowConfig::height) / 2;
+  glfwSetCursorPosCallback(
+      window, [](GLFWwindow *win, double xpos, double ypos) {
+        static bool firstMouse = true;
+        static float lastX = static_cast<float>(WindowConfig::width) / 2,
+                     lastY = static_cast<float>(WindowConfig::height) / 2;
 
-    if (firstMouse) {
-      lastX = xpos;
-      lastY = ypos;
-      firstMouse = false;
-    }
+        if (firstMouse) {
+          lastX = xpos;
+          lastY = ypos;
+          firstMouse = false;
+        }
 
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;  // y координата перевернута
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos; // y координата перевернута
 
-    lastX = xpos;
-    lastY = ypos;
+        lastX = xpos;
+        lastY = ypos;
 
-    auto *cam = static_cast<Camera *>(glfwGetWindowUserPointer(win));
-    if (cam) {
-      cam->ProcessMouseMovement(xoffset, yoffset);
-    }
-  });
+        auto *cam = static_cast<Camera *>(glfwGetWindowUserPointer(win));
+        if (cam) {
+          cam->ProcessMouseMovement(xoffset, yoffset);
+        }
+      });
 
-  glfwSetScrollCallback(window, [](GLFWwindow *win, double xoffset, double yoffset) {
-    auto *cam = static_cast<Camera *>(glfwGetWindowUserPointer(win));
-    if (cam) {
-      cam->ProcessMouseScroll(yoffset);
-    }
-  });
+  glfwSetScrollCallback(
+      window, [](GLFWwindow *win, double xoffset, double yoffset) {
+        auto *cam = static_cast<Camera *>(glfwGetWindowUserPointer(win));
+        if (cam) {
+          cam->ProcessMouseScroll(yoffset);
+        }
+      });
 
-  Shader ourShader("../src/source/ShProgram/vertex_model.sh",
-                   "../src/source/ShProgram/fragment_model.sh");
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS); // тест глубины всегда проходит успешно (аналогично
+                        // glDisable(GL_DEPTH_TEST))
 
-  Model ourModel("../src/source/Model/backpack/backpack.obj");
+  Shader shader("../src/source/ShProgram/vertex_depth_test.sh",
+                "../src/source/ShProgram/fragment_depth_test.sh");
+
+  float cubeVertices[] = {
+      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,
+      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,
+
+      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,
+
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,
+      0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 1.0f,
+      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
+
+  float planeVertices[] = {
+
+      5.0f, -0.5f, 5.0f,  2.0f,  0.0f,  -5.0f, -0.5f, 5.0f,
+      0.0f, 0.0f,  -5.0f, -0.5f, -5.0f, 0.0f,  2.0f,
+
+      5.0f, -0.5f, 5.0f,  2.0f,  0.0f,  -5.0f, -0.5f, -5.0f,
+      0.0f, 2.0f,  5.0f,  -0.5f, -5.0f, 2.0f,  2.0f};
+
+  GLuint cubeVAO, cubeVBO;
+  glGenVertexArrays(1, &cubeVAO);
+  glGenBuffers(1, &cubeVBO);
+  glBindVertexArray(cubeVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices,
+               GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glBindVertexArray(0);
+
+  GLuint planeVAO, planeVBO;
+  glGenVertexArrays(1, &planeVAO);
+  glGenBuffers(1, &planeVBO);
+  glBindVertexArray(planeVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices,
+               GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glBindVertexArray(0);
+
+  GLuint cubeTexture = LoadTexture("../src/texture/marble.jpg");
+  GLuint floorTexture = LoadTexture("../src/texture/metal.jpg");
+
+  shader.use();
+  shader.setInt("texture1", 0);
 
   while (!glfwWindowShouldClose(window)) {
     float currentFrame = glfwGetTime();
@@ -89,29 +163,41 @@ int main() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Отрисовка куба
-    ourShader.use();
-
-    const glm::mat4 &view = camera.GetViewMatrix();
-    glm::mat4 projection =
-        glm::perspective(glm::radians(camera.GetZoom()),
-                         (float)WindowConfig::width / (float)WindowConfig::height, 0.1f, 100.0f);
-
-    ourShader.setMat4("projection", projection);
-    ourShader.setMat4("view", view);
-
+    shader.use();
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model,
-                           glm::vec3(0.0f, 0.0f, 0.0f));  // смещаем вниз чтобы быть в центре сцены
-    model = glm::scale(
-        model,
-        glm::vec3(1.0f, 1.0f,
-                  1.0f));  // объект слишком большой для нашей сцены, поэтому немного уменьшим его
-    ourShader.setMat4("model", model);
-    ourModel.Draw(ourShader);
+    const glm::mat4 &view = camera.GetViewMatrix();
+    glm::mat4 projection = glm::perspective(
+        glm::radians(camera.GetZoom()),
+        (float)WindowConfig::width / (float)WindowConfig::height, 0.1f, 100.0f);
+
+    shader.setMat4("projection", projection);
+    shader.setMat4("view", view);
+
+    glBindVertexArray(cubeVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, cubeTexture);
+    model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+    shader.setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+    shader.setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    glBindVertexArray(planeVAO);
+    glBindTexture(GL_TEXTURE_2D, floorTexture);
+    shader.setMat4("model", glm::mat4(1.0f));
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  glDeleteVertexArrays(1, &cubeVAO);
+  glDeleteVertexArrays(1, &planeVAO);
+  glDeleteBuffers(1, &cubeVBO);
+  glDeleteBuffers(1, &planeVBO);
 
   glfwTerminate();
 
